dd564e24ded204020412fdc035c3f9b86221a4fe
cpunkzzz <cpunkzzz@gmail.com>
2022-08-16 20:25:52 -0400

    read a stark proof

--- methods/guest/src/bin/multiply.rs
+++ methods/guest/src/bin/multiply.rs
@@ -1,20 +1,23 @@
 #![no_main]
 #![no_std]
+use winter_air::proof::StarkProof;
+// use miden_verifier::verify;
 
 use risc0_zkvm_guest::env;
 
 risc0_zkvm_guest::entry!(main);
 
 pub fn main() {
-    // Load the first number from the host
-    let a: u64 = env::read();
-    // Load the second number from the host
-    let b: u64 = env::read();
-    // Verify that neither of them are 1 (i.e. nontrivial factors)
-    if a == 1 || b == 1 {
-        panic!("Trivial factors")
-    }
-    // Compute the product while being careful with integer overflow
-    let product = a.checked_mul(b).expect("Integer overflow");
-    env::commit(&product);
+    let proof: StarkProof = env::read();
+    // // Load the first number from the host
+    // let a: u64 = env::read();
+    // // Load the second number from the host
+    // let b: u64 = env::read();
+    // // Verify that neither of them are 1 (i.e. nontrivial factors)
+    // if a == 1 || b == 1 {
+    //     panic!("Trivial factors")
+    // }
+    // // Compute the product while being careful with integer overflow
+    // let product = a.checked_mul(b).expect("Integer overflow");
+    // env::commit(&product);
 }
--- starter/src/main.rs
+++ starter/src/main.rs
@@ -1,29 +1,67 @@
 use methods::{MULTIPLY_ID, MULTIPLY_PATH};
+use miden::ProofOptions;
 use risc0_zkvm::host::Prover;
 use risc0_zkvm::serde::{from_slice, to_vec};
 
-fn main() {
-    // Pick two numbers
-    let a: u64 = 17;
-    let b: u64 = 23;
+pub mod fibonacci;
 
-    // Multiply them inside the ZKP
-    // First, we make the prover, loading the 'multiply' method
+fn main() {
+    println!("============================================================");
+
+    let proof_options = get_proof_options();
+
+    // instantiate and prepare the example
+    let example = fibonacci::get_example(1024);
+
+    let fibonacci::Example {
+        program,
+        inputs,
+        num_outputs,
+        pub_inputs,
+        expected_result,
+    } = example;
+    println!("--------------------------------");
+
+    // execute the program and generate the proof of execution
+    let (outputs, proof) = miden::prove(&program, &inputs, num_outputs, &proof_options).unwrap();
+    println!("--------------------------------");
+    println!("Program output: {:?}", outputs);
+    assert_eq!(
+        expected_result, outputs,
+        "Program result was computed incorrectly"
+    );
+
     let mut prover = Prover::new(&std::fs::read(MULTIPLY_PATH).unwrap(), MULTIPLY_ID).unwrap();
-    // Next we send a & b to the guest
-    prover.add_input(to_vec(&a).unwrap().as_slice()).unwrap();
-    prover.add_input(to_vec(&b).unwrap().as_slice()).unwrap();
-    // Run prover & generate receipt
+    prover
+        .add_input(to_vec(&proof).unwrap().as_slice())
+        .unwrap();
     let receipt = prover.run().unwrap();
 
-    // Extract journal of receipt (i.e. output c, where c = a * b)
-    let c: u64 = from_slice(&receipt.get_journal_vec().unwrap()).unwrap();
+    // // Pick two numbers
+    // let a: u64 = 17;
+    // let b: u64 = 23;
 
-    // Print an assertion
-    println!("I know the factors of {}, and I can prove it!", c);
+    // // Multiply them inside the ZKP
+    // // First, we make the prover, loading the 'multiply' method
+    // let mut prover = Prover::new(&std::fs::read(MULTIPLY_PATH).unwrap(), MULTIPLY_ID).unwrap();
+    // // Next we send a & b to the guest
+    // prover.add_input(to_vec(&a).unwrap().as_slice()).unwrap();
+    // prover.add_input(to_vec(&b).unwrap().as_slice()).unwrap();
+    // // Run prover & generate receipt
+    // let receipt = prover.run().unwrap();
 
-    // Here is where one would send 'receipt' over the network...
+    // // Extract journal of receipt (i.e. output c, where c = a * b)
+    // let c: u64 = from_slice(&receipt.get_journal_vec().unwrap()).unwrap();
 
-    // Verify receipt, panic if it's wrong
-    receipt.verify(MULTIPLY_ID).unwrap();
+    // // Print an assertion
+    // println!("I know the factors of {}, and I can prove it!", c);
+
+    // // Here is where one would send 'receipt' over the network...
+
+    // // Verify receipt, panic if it's wrong
+    // receipt.verify(MULTIPLY_ID).unwrap();
+}
+
+pub fn get_proof_options() -> ProofOptions {
+    ProofOptions::with_96_bit_security()
 }
