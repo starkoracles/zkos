ff4737b95a4e46477b8eda0cd499d23a14a9d45e
cpunkzzz <cpunkzzz@gmail.com>
2022-08-19 15:46:30 -0400

    working rkyv

--- methods/guest/src/bin/multiply.rs
+++ methods/guest/src/bin/multiply.rs
@@ -1,18 +1,33 @@
 #![no_main]
 #![no_std]
-use winter_air::proof::StarkProof;
+use winter_air::proof::{Commitments, Context, OodFrame, Queries};
 // use miden_verifier::verify;
 
 use risc0_zkvm_guest::env;
 
 risc0_zkvm_guest::entry!(main);
+use rkyv::{Archive, Deserialize, Serialize};
 
+#[derive(Archive, Deserialize, Serialize, Debug, PartialEq)]
+// This will generate a PartialEq impl between our unarchived and archived types
+#[archive(compare(PartialEq))]
+// We can pass attributes through to generated types with archive_attr
+#[archive_attr(derive(Debug))]
+struct ProgArgs {
+    pub a: u64,
+    pub b: u64,
+}
 pub fn main() {
-    let proof: StarkProof = env::read();
-    // // Load the first number from the host
-    // let a: u64 = env::read();
+    let arg_bytes: &[u8] = env::read_raw();
+    let archived = unsafe { rkyv::archived_root::<ProgArgs>(&arg_bytes[..]) };
+    assert_eq!(context.trace_length(), 4096);
+    // let commitments: Commitments = env::read();
+    // let ood_frame: OodFrame = env::read();
+    // Load the first number from the host
+    // let args: ProgArgs = env::read();
+    // let a: u64 = args.a;
     // // Load the second number from the host
-    // let b: u64 = env::read();
+    // let b: u64 = args.b;
     // // Verify that neither of them are 1 (i.e. nontrivial factors)
     // if a == 1 || b == 1 {
     //     panic!("Trivial factors")
--- starter/src/main.rs
+++ starter/src/main.rs
@@ -2,6 +2,17 @@ use methods::{MULTIPLY_ID, MULTIPLY_PATH};
 use miden::ProofOptions;
 use risc0_zkvm::host::Prover;
 use risc0_zkvm::serde::{from_slice, to_vec};
+use rkyv::{Archive, Deserialize, Serialize};
+
+#[derive(Archive, Deserialize, Serialize, Debug, PartialEq)]
+// This will generate a PartialEq impl between our unarchived and archived types
+#[archive(compare(PartialEq))]
+// We can pass attributes through to generated types with archive_attr
+#[archive_attr(derive(Debug))]
+struct ProgArgs {
+    pub a: u64,
+    pub b: u64,
+}
 
 pub mod fibonacci;
 
@@ -25,6 +36,31 @@ fn main() {
     // execute the program and generate the proof of execution
     let (outputs, proof) = miden::prove(&program, &inputs, num_outputs, &proof_options).unwrap();
     println!("--------------------------------");
+    println!("Trace length: {}", proof.context.trace_length());
+    println!(
+        "context num of words: {}",
+        to_vec(&proof.context).unwrap().as_slice().len()
+    );
+    println!(
+        "commitments num of words: {}",
+        to_vec(&proof.commitments).unwrap().as_slice().len()
+    );
+    println!(
+        "trace query num of words: {}",
+        to_vec(&proof.trace_queries).unwrap().as_slice().len()
+    );
+    println!(
+        "Constraint query num of words: {}",
+        to_vec(&proof.constraint_queries).unwrap().as_slice().len()
+    );
+    println!(
+        "OOD frame query num of words: {}",
+        to_vec(&proof.ood_frame).unwrap().as_slice().len()
+    );
+    println!(
+        "fri proof num of words: {}",
+        to_vec(&proof.fri_proof).unwrap().as_slice().len()
+    );
     println!("Program output: {:?}", outputs);
     assert_eq!(
         expected_result, outputs,
@@ -32,21 +68,33 @@ fn main() {
     );
 
     let mut prover = Prover::new(&std::fs::read(MULTIPLY_PATH).unwrap(), MULTIPLY_ID).unwrap();
-    prover
-        .add_input(to_vec(&proof).unwrap().as_slice())
-        .unwrap();
+    let args = ProgArgs { a: 17, b: 23 };
+    let bytes = rkyv::to_bytes::<_, 256>(&args).unwrap();
+    prover.add_input_u8_slice(&bytes);
+    let archived = unsafe { rkyv::archived_root::<ProgArgs>(&bytes[..]) };
+    println!("archived: {}, {}", archived.a, archived.b);
+    // prover
+    //     .add_input(to_vec(&proof.context).unwrap().as_slice())
+    //     .unwrap();
+    // prover
+    //     .add_input(to_vec(&proof.commitments).unwrap().as_slice())
+    //     .unwrap();
+    // prover
+    //     .add_input(to_vec(&proof.ood_frame).unwrap().as_slice())
+    //     .unwrap();
     let receipt = prover.run().unwrap();
+    receipt.verify(MULTIPLY_ID).unwrap();
 
     // // Pick two numbers
     // let a: u64 = 17;
     // let b: u64 = 23;
+    // let args = ProgArgs { a, b };
 
     // // Multiply them inside the ZKP
     // // First, we make the prover, loading the 'multiply' method
     // let mut prover = Prover::new(&std::fs::read(MULTIPLY_PATH).unwrap(), MULTIPLY_ID).unwrap();
     // // Next we send a & b to the guest
-    // prover.add_input(to_vec(&a).unwrap().as_slice()).unwrap();
-    // prover.add_input(to_vec(&b).unwrap().as_slice()).unwrap();
+    // prover.add_input(to_vec(&args).unwrap().as_slice()).unwrap();
     // // Run prover & generate receipt
     // let receipt = prover.run().unwrap();
 
