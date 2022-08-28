24d3c70674547a96c3d224d8e6381cce11705a65
cpunkzzz <cpunkzzz@gmail.com>
2022-08-27 20:30:17 -0400

    Add more verifier, trace length dominated by blake192

--- recursive/methods/guest/src/bin/recursive.rs
+++ recursive/methods/guest/src/bin/recursive.rs
@@ -3,17 +3,38 @@
 extern crate alloc;
 
 use alloc::vec::Vec;
+use anyhow::{anyhow, Result};
 use miden_air::ProcessorAir;
 use risc0_zkvm_guest::env;
+use rkyv::Archive;
 use utils::inputs::{AirInput, RiscInput};
 use winter_air::{
     proof::{Commitments, Context, OodFrame, Queries, StarkProof},
-    Air,
+    Air, AuxTraceRandElements,
 };
 use winter_crypto::{hashers::Blake3_192, ByteDigest, RandomCoin};
 use winter_math::fields::{f64::BaseElement, QuadExtension};
 
 risc0_zkvm_guest::entry!(main);
+
+pub fn aux_trace_segments(
+    risc_input: &<RiscInput as Archive>::Archived,
+    public_coin: &mut RandomCoin<BaseElement, Blake3_192<BaseElement>>,
+    air: &ProcessorAir,
+) -> Result<()> {
+    let first_digest = ByteDigest::new(risc_input.trace_commitments[0]);
+    public_coin.reseed(first_digest);
+    let mut aux_trace_rand_elements = AuxTraceRandElements::<BaseElement>::new();
+    for (i, commitment) in risc_input.trace_commitments.iter().skip(1).enumerate() {
+        let rand_elements = air
+            .get_aux_trace_segment_random_elements(i, public_coin)
+            .map_err(|_| anyhow!("Random coin error"))?;
+        aux_trace_rand_elements.add_segment_elements(rand_elements);
+        let c = ByteDigest::new(*commitment);
+        public_coin.reseed(c);
+    }
+    Ok(())
+}
 
 pub fn main() {
     let aux_input: &[u8] = env::read_aux_input();
@@ -28,22 +49,6 @@ pub fn main() {
     let public_coin_seed = Vec::new();
     let mut public_coin: RandomCoin<BaseElement, Blake3_192<BaseElement>> =
         RandomCoin::new(&public_coin_seed);
-    let first_digest = ByteDigest::new(risc_input.trace_commitments[0]);
-    public_coin.reseed(first_digest);
-
-    // assert_eq!(archived.context.trace_length(), 4096);
-    // let commitments: Commitments = env::read();
-    // let ood_frame: OodFrame = env::read();
-    // Load the first number from the host
-    // let args: ProgArgs = env::read();
-    // let a: u64 = args.a;
-    // // Load the second number from the host
-    // let b: u64 = args.b;
-    // // Verify that neither of them are 1 (i.e. nontrivial factors)
-    // if a == 1 || b == 1 {
-    //     panic!("Trivial factors")
-    // }
-    // // Compute the product while being careful with integer overflow
-    // let product = a.checked_mul(b).expect("Integer overflow");
-    // env::commit(&product);
+    // process auxiliary trace segments (if any), to build a set of random elements for each segment
+    aux_trace_segments(&risc_input, &mut public_coin, &air).expect("aux trace segments failed");
 }
